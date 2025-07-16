0f1716f18738932d617166f642b8b3aa057acacb
Ritwikgotbugs <shritwik04@gmail.com>
2025-07-16 11:59:35 +0530

    fix: dburl

--- Eco backend/lib/models/index.js
+++ Eco backend/lib/models/index.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ecostock', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-});
-
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 module.exports = sequelize; 
